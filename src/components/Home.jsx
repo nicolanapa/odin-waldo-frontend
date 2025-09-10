a22a8d92418d84f2482fa2deb19ed54539ed86d3
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-09-10 21:59:39 +0200

    feat: Test Photo with toggle method that calls onClick

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react";
+import TargetBox from "./TargetBox";
+
 function Home() {
-    return <></>;
+    const [showTargetBox, setShowTargetBox] = useState(false);
+
+    const toggleTargetBox = () => {
+        setShowTargetBox(!showTargetBox);
+    };
+
+    return (
+        <main>
+            {showTargetBox && <TargetBox />}
+
+            <div className="image-container">
+                <img
+                    src="https://cdn.pixabay.com/photo/2025/08/12/11/55/mallards-9770059_1280.jpg"
+                    alt="Test Photo"
+                    width="90%"
+                    height="auto"
+                    onClick={toggleTargetBox}
+                />
+            </div>
+        </main>
+    );
 }
 
 export default Home;
