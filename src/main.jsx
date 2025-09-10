0621c2da98a7f8ec7d930ba747ed7d6b1f94b125
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-09-10 21:19:02 +0200

    feat: Updated formatting and path of style.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./style.css";
+
 import Home from "./components/Home";
+
+import "./styles/style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
