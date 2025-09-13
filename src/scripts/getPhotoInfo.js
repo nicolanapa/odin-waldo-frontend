12acbc6661dde65426e28f265bfd535748e5c82e
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-09-13 23:34:42 +0200

    feat: Simple Helper to get a specific Photo

---
+++ src/scripts/getPhotoInfo.js
@@ -0,0 +1,11 @@
+async function getPhotoInfo(id) {
+    const photo = await fetch(
+        import.meta.env.VITE_FULL_HOSTNAME + "/photo/" + id
+    ).then((res) => res.json());
+
+    console.log(photo);
+
+    return photo;
+}
+
+export default getPhotoInfo;
