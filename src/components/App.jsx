70732ac46a0452ef216af848a352e0b38342d432
nicolanapa <126412131+nicolanapa@users.noreply.github.com>
2025-09-13 23:37:30 +0200

    feat: Fetching of all photo IDs and a single full photo to be displayed randomly

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,33 @@
+import { useEffect, useState } from "react";
 import ImageContainer from "./ImageContainer";
+import getPhotoInfo from "../scripts/getPhotoInfo";
 
 function App() {
-    // Fetch from GET /photo and pass it to ImageContainer
-    // Use Context?
+    const [photoIDs, setPhotoIDs] = useState([]);
+    const [currentImage, setCurrentImage] = useState(false);
+
+    useEffect(
+        () => async () => {
+            const photoIDsArray = await fetch(
+                import.meta.env.VITE_FULL_HOSTNAME + "/photo/"
+            ).then((res) => res.json());
+
+            setPhotoIDs(photoIDsArray);
+
+            const randomPhotoId = Math.floor(Math.random() * photoIDs.length);
+
+            setCurrentImage(await getPhotoInfo(photoIDsArray[randomPhotoId]));
+        },
+        []
+    );
+
+    // TODO: Automatically change an Image when it's finished
 
     return (
         <main>
-            <ImageContainer />
+            {photoIDs.length !== 0
+                ? currentImage && <ImageContainer image={currentImage} />
+                : "No Image Available"}
         </main>
     );
 }
